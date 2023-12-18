841dbb463f38bc53e9491f3b0a04aa56d8563b35
samohianvera <samohinavera@gmail.com>
2023-12-18 18:39:48 +0300

    fixed settings

--- config/settings.py
+++ config/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'HOST': os.getenv('POSTGRES_HOST'),
     }
 }
 # Password validation
